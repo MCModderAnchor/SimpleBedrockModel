48d7b21e62408452b73d1dd1b62f98c7c152bc63
xjqsh <59763518+xjqsh@users.noreply.github.com>
2026-05-16 17:59:22 +0800

    修正测试配置，移除错误的sodium poly mesh写入

--- src/main/java/com/github/mcmodderanchor/simplebedrockmodel/v1/common/model/BedrockModel.java
+++ src/main/java/com/github/mcmodderanchor/simplebedrockmodel/v1/common/model/BedrockModel.java
@@ -149,12 +149,12 @@ public class BedrockModel implements Skeleton, BoneIndexProvider {
     }
 
     protected BedrockMesh createPolyMesh(PolyMeshItem polyMesh, BedrockBone part, float texWidth, float texHeight) {
-        if (SodiumCompat.isSodiumInstalled()) {
-            return new SodiumBedrockPolyMesh(polyMesh, part, texWidth, texHeight);
-        }
-        if (EmbeddiumCompat.isEmbeddiumInstalled()) {
-            return new EmbeddiumBedrockPolyMesh(polyMesh, part, texWidth, texHeight);
-        }
+//        if (SodiumCompat.isSodiumInstalled()) {
+//            return new SodiumBedrockPolyMesh(polyMesh, part, texWidth, texHeight);
+//        }
+//        if (EmbeddiumCompat.isEmbeddiumInstalled()) {
+//            return new EmbeddiumBedrockPolyMesh(polyMesh, part, texWidth, texHeight);
+//        }
         return new BedrockPolyMesh(polyMesh, part, texWidth, texHeight);
     }
 
