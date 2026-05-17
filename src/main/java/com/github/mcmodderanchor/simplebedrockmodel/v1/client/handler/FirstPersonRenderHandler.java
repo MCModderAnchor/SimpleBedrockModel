3235027b0d3e843ba20aa9857a700ac62b815018
xjqsh <59763518+xjqsh@users.noreply.github.com>
2026-05-17 21:49:56 +0800

    修正有笨蛋把主手又画了两遍的问题

--- src/main/java/com/github/mcmodderanchor/simplebedrockmodel/v1/client/handler/FirstPersonRenderHandler.java
+++ src/main/java/com/github/mcmodderanchor/simplebedrockmodel/v1/client/handler/FirstPersonRenderHandler.java
@@ -223,6 +223,8 @@ public class FirstPersonRenderHandler {
         IFPGeoItemRenderer renderer = optRenderer.get();
         if (event.getHand() == InteractionHand.OFF_HAND && renderer.blockOffhandRender()) {
             event.setCanceled(true);
+            event.setCanceled(true);
+            return;
         }
 
         ItemDisplayContext transformType = ItemDisplayContext.FIRST_PERSON_RIGHT_HAND;
